f306d871a4afc704bb8a3dfbfad8a22264126eed
Elias Almqvist <elalmqvist@gmail.com>
2024-06-12 15:11:14 +0200

    add neovim to home config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -38,14 +38,13 @@
     };
   };
 
-  # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "elal";
+    homeDirectory = "/home/elal";
   };
 
   # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
+  programs.neovim.enable = true;
   # home.packages = with pkgs; [ steam ];
 
   # Enable home-manager and git
