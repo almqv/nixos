a10ff5534a5b1a654f5f3ee379c23aa46ec219c5
Elias Almqvist <elalmqvist@gmail.com>
2024-06-15 20:46:31 +0200

    fix: deprecated namings for xkb

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -91,9 +91,9 @@
   services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "se";
-    xkbVariant = "";
+    variant = "";
   };
 
   # Configure console keymap
